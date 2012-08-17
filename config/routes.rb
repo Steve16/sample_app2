b851a5b8c667eb5b48054d8b7faaa91db8b07975
Steve <stevenroger@comcast.net>
2012-08-16 17:09:52 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp2::Application.routes.draw do
-  get "users/new"
+  resources :users #creates all the named paths for the users object
 
   root to: 'static_pages#home'
 
@@ -7,7 +7,9 @@ SampleApp2::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact' 
 
-  match '/signup', to: 'users#new' 
+  match '/signup', to: 'users#new'
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -95,11 +95,13 @@ describe "return value of authenticate method" do
     specify { user_for_invalid_password.should be_false }
   end
 
-describe "with a password that's too short" do
-  before { @user.password = @user.password_confirmation = "a" * 5 }
-  it { should be_invalid }
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
 end
 
-end
+
+
 
 end
